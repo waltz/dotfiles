ebe3bcae423b67ed30c7cd3281901138d413967a
CJ Bryan <cj@getwellio.com>
2020-06-04 12:22:04 -0700

    Power CtrlP with The Silver Searcher

--- setup.sh
+++ setup.sh
@@ -8,6 +8,10 @@ ln -s ~/Code/dotfiles/vimrc ~/.vimrc
 mv ~/.zshrc ~/.zshrc.old
 ln -s ~/Code/dotfiles/zshrc ~/.zshrc
 
+# Setup The Silver Searcher Ignore file
+mv ~/.ignore ~/.ignore.old
+ln -s ~/Code/dotfiles/.ignore ~/.ignore
+
 # OSX key repeat settings
 defaults write NSGlobalDomain KeyRepeat -int 0
 defaults write NSGlobalDomain InitialKeyRepeat -int 0
