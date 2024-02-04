30e7bae6068a5302d336b84d5134bd2d738502bd
CJ Bryan <cj@cjbryan.io>
2024-02-03 21:29:05 -0800

    Add some sane autocomplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,10 @@ require("lazy").setup({
   { "williamboman/mason.nvim" },
   { "williamboman/mason-lspconfig.nvim" },
   { "neovim/nvim-lspconfig" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/nvim-cmp" },
   { "airblade/vim-gitgutter" },
   { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { 'nvim-lua/plenary.nvim' } },
   { "nvim-treesitter/nvim-treesitter" },
@@ -33,10 +37,26 @@ require('mason-lspconfig').setup({
   }
 })
 require('mason-lspconfig').setup_handlers {
-  function (server_name) 
+  function (server_name)
     require('lspconfig')[server_name].setup {}
   end,
 }
+
+local cmp = require("cmp")
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
 
 vim.opt.number = true
 
